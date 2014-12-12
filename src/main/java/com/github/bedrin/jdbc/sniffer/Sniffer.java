3e72f4b69eb12c41379c8b2f1555963909ef1ac9
Dmitry Bedrin <Dmitry.Bedrin@gmail.com>
2014-12-12 14:35:19 +0300

    Added verify methods to Sniffer

--- src/main/java/com/github/bedrin/jdbc/sniffer/Sniffer.java
+++ src/main/java/com/github/bedrin/jdbc/sniffer/Sniffer.java
@@ -17,5 +17,19 @@ public class Sniffer {
     public static void reset() {
         counter.set(0);
     }
+    
+    public static void verifyNotMore() {
+        verifyNotMoreThan(0);
+    }
+
+    public static void verifyNotMoreThanOne() {
+        verifyNotMoreThan(1);
+    }
+
+    public static void verifyNotMoreThan(int allowedStatements) throws IllegalStateException {
+        if (executedStatements() > allowedStatements)
+            throw new IllegalStateException(String.format("Allowed not more than %d statements, but actually caught %d statements", allowedStatements, actualStatements));
+        reset();
+    }    
 
 }
