f5ee3b70f24a65cec316809aa1e243974dc28fc8
bedrin <Dmitry.Bedrin@gmail.com>
2014-12-19 00:49:53 +0300

    Added javadoc to the ThreadLocalSniffer class since it is a public API

--- src/main/java/com/github/bedrin/jdbc/sniffer/ThreadLocalSniffer.java
+++ src/main/java/com/github/bedrin/jdbc/sniffer/ThreadLocalSniffer.java
@@ -1,5 +1,9 @@
 package com.github.bedrin.jdbc.sniffer;
 
+/**
+ * Sniffer holds the number of executed queries in current thread and provides static methods for accessing them
+ * @since 1.0
+ */
 public class ThreadLocalSniffer extends ThreadLocal<Sniffer> {
 
     private final static ThreadLocalSniffer INSTANCE = new ThreadLocalSniffer();
@@ -9,26 +13,51 @@ public class ThreadLocalSniffer extends ThreadLocal<Sniffer> {
         return new Sniffer();
     }
 
-    public static void executeStatement() {
+    static void executeStatement() {
         INSTANCE.get().executeStatementImpl();
     }
 
+    /**
+     * @return the number of executed queries in current thread since the last call of {@link #reset()} method or to
+     * any of verify methods family like {@link #verifyNotMore)}, {@link #verifyNotMoreThanOne)}
+     * or {@link #verifyNotMoreThanOne)}
+     */
     public static int executedStatements() {
         return INSTANCE.get().executedStatementsImpl();
     }
 
+    /**
+     * Resets the queries counter to 0
+     */
     public static void reset() {
         INSTANCE.get().resetImpl();
     }
 
+    /**
+     * Verifies that no queries has been executed in current thread since the last call of {@link #reset()} method
+     * or to any of verify methods family
+     * @throws IllegalStateException if actual number of executed statements exceeded 0
+     */
     public static void verifyNotMore() {
         verifyNotMoreThan(0);
     }
 
+    /**
+     * Verifies that at most 1 query has been executed in current thread since the last call of {@link #reset()} method
+     * or to any of verify methods family
+     * @throws IllegalStateException if actual number of executed statements exceeded 1
+     */
     public static void verifyNotMoreThanOne() {
         verifyNotMoreThan(1);
     }
 
+    /**
+     * Verifies that at most {@code allowedStatements} query has been executed  in current thread since the last call
+     * of {@link #reset()} method or to any of verify methods family
+     * @param allowedStatements maximum number of statements which could have been executed previously since
+     *                          last {@link #reset()} call
+     * @throws IllegalStateException if actual number of executed statements exceeded {@code allowedStatements}
+     */
     public static void verifyNotMoreThan(int allowedStatements) throws IllegalStateException {
         int actualStatements = executedStatements();
         if (actualStatements > allowedStatements)
