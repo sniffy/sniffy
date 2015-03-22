3e71e13a12e2ae256220c5ccbffea450f5117b78
bedrin <Dmitry.Bedrin@gmail.com>
2015-03-22 12:03:04 +0300

    Version bumped to 2.0

--- src/main/java/com/github/bedrin/jdbc/sniffer/Constants.java
+++ src/main/java/com/github/bedrin/jdbc/sniffer/Constants.java
@@ -2,6 +2,6 @@ package com.github.bedrin.jdbc.sniffer;
 
 public final class Constants {
     public final static String DRIVER_PREFIX = "sniffer:";
-    public final static int MAJOR_VERSION = 1;
-    public final static int MINOR_VERSION = 5;
+    public final static int MAJOR_VERSION = 2;
+    public final static int MINOR_VERSION = 0;
 }
