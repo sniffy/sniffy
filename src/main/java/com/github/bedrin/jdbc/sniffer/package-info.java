08b18b1bd5100beb028ff5e069b317679d2880d5
bedrin <Dmitry.Bedrin@gmail.com>
2015-04-14 00:49:47 +0300

    Added overall brief description to package-info javadoc

--- src/main/java/com/github/bedrin/jdbc/sniffer/package-info.java
+++ src/main/java/com/github/bedrin/jdbc/sniffer/package-info.java
@@ -1,4 +1,17 @@
 /**
- *
+ * JDBC Sniffer allows you to validate the number of SQL queries executed by a given block of code
+ * Example usage:
+ * <pre>
+ * {@code
+ * @Test
+ * public void testFunctionalApi() throws SQLException {
+ *     final Connection connection = DriverManager.getConnection("sniffer:jdbc:h2:~/test", "sa", "sa");
+ *     // Sniffer.execute() method executes the lambda expression and returns an instance of RecordedQueries
+ *     // this class provides methods for validating the number of executed queries
+ *     Sniffer.execute(() -> connection.createStatement().execute("SELECT 1 FROM DUAL")).verifyNotMoreThanOne();
+ * }
+ * }
+ * </pre>
+ * @see com.github.bedrin.jdbc.sniffer.Sniffer for the detailed API description
  */
 package com.github.bedrin.jdbc.sniffer;
