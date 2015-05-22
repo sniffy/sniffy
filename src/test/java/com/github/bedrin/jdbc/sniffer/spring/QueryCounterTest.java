6cfe3452b2160f2d86c427ddd9fc3dcbb80ab247
bedrin <Dmitry.Bedrin@gmail.com>
2015-05-22 21:34:06 +0300

    Added unit test for spring integration

---
+++ src/test/java/com/github/bedrin/jdbc/sniffer/spring/QueryCounterTest.java
@@ -0,0 +1,29 @@
+package com.github.bedrin.jdbc.sniffer.spring;
+
+import com.github.bedrin.jdbc.sniffer.BaseTest;
+import com.github.bedrin.jdbc.sniffer.WrongNumberOfQueriesError;
+import com.github.bedrin.jdbc.sniffer.junit.NoQueriesAllowed;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@NoQueriesAllowed
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = QueryCounterTest.class)
+@TestExecutionListeners(QueryCounterListener.class)
+public class QueryCounterTest extends BaseTest {
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testNotAllowedQueriesByDefault() {
+        executeStatement();
+        thrown.expect(WrongNumberOfQueriesError.class);
+    }
+
+}
