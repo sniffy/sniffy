36d0ddd894e913d06f9d55b74482b713c7077ec1
bedrin <Dmitry.Bedrin@gmail.com>
2015-08-05 00:40:27 +0300

    Servlet 2.3 compatibility

--- src/main/java/com/github/bedrin/jdbc/sniffer/servlet/SnifferServlet.java
+++ src/main/java/com/github/bedrin/jdbc/sniffer/servlet/SnifferServlet.java
@@ -20,8 +20,6 @@ public class SnifferServlet extends HttpServlet {
 
     protected final Map<String, List<StatementMetaData>> cache;
 
-    protected String contextPath;
-
     protected byte[] javascript;
     protected byte[] css;
 
@@ -31,7 +29,6 @@ public class SnifferServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        contextPath = config.getServletContext().getContextPath();
         try {
             javascript = loadResource("jdbcsniffer.min.js");
         } catch (IOException e) {
@@ -46,7 +43,7 @@ public class SnifferServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path = request.getRequestURI().substring(contextPath.length());
+        String path = request.getRequestURI().substring(request.getContextPath().length());
 
         if (SnifferFilter.JAVASCRIPT_URI.equals(path)) {
             serveContent(response, "application/javascript", javascript);
