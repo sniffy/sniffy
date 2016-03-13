679ffb5da93df459aa1bd19bbbc19d59af0fabcf
bedrin <passw0rd>
2016-03-13 20:08:10 +0300

    Added more unit tests for SnifferSocket wrapper

--- src/test/java/io/sniffy/socket/SnifferSocketImplTest.java
+++ src/test/java/io/sniffy/socket/SnifferSocketImplTest.java
@@ -7,10 +7,15 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.SocketImpl;
 
-import static org.powermock.api.mockito.PowerMockito.verifyNoMoreInteractions;
-import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyInt;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(SocketImpl.class)
@@ -37,12 +42,106 @@ public class SnifferSocketImplTest {
     }
 
     @Test
+    public void testSendUrgentDataThrowIOException() throws Exception {
+
+        IOException expected = new IOException();
+        when(delegate, "sendUrgentData", anyInt()).thenThrow(expected);
+
+        try {
+            sniffySocket.sendUrgentData(1);
+            fail();
+        } catch (IOException actual) {
+            assertEquals(expected, actual);
+        }
+
+        verifyPrivate(delegate).invoke("sendUrgentData",1);
+        verifyNoMoreInteractions(delegate);
+
+    }
+
+    @Test
+    public void testSendUrgentDataThrowRuntimeException() throws Exception {
+
+        RuntimeException expected = new RuntimeException();
+        when(delegate, "sendUrgentData", anyInt()).thenThrow(expected);
+
+        try {
+            sniffySocket.sendUrgentData(1);
+            fail();
+        } catch (Exception actual) {
+            assertEquals(expected, actual);
+        }
+
+        verifyPrivate(delegate).invoke("sendUrgentData",1);
+        verifyNoMoreInteractions(delegate);
+
+    }
+
+    @Test
     public void testShutdownInput() throws Exception {
 
         sniffySocket.shutdownInput();
 
         verifyPrivate(delegate).invoke("shutdownInput");
         verifyNoMoreInteractions(delegate);
+
+    }
+
+    @Test
+    public void testShutdownOutput() throws Exception {
+
+        sniffySocket.shutdownOutput();
+
+        verifyPrivate(delegate).invoke("shutdownOutput");
+        verifyNoMoreInteractions(delegate);
+
+    }
+
+    @Test
+    public void testGetFileDescriptor() throws Exception {
+
+        FileDescriptor expected = mock(FileDescriptor.class);
+
+        when(delegate, "getFileDescriptor").thenReturn(expected);
+
+        FileDescriptor actual = sniffySocket.getFileDescriptor();
+
+        verifyPrivate(delegate).invoke("getFileDescriptor");
+        verifyNoMoreInteractions(delegate);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testGetInetAddress() throws Exception {
+
+        InetAddress expected = mock(InetAddress.class);
+
+        when(delegate, "getInetAddress").thenReturn(expected);
+
+        InetAddress actual = sniffySocket.getInetAddress();
+
+        verifyPrivate(delegate).invoke("getInetAddress");
+        verifyNoMoreInteractions(delegate);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void getPort() throws Exception {
+
+        int expected = 1;
+
+        when(delegate, "getPort").thenReturn(expected);
+
+        int actual = sniffySocket.getPort();
+
+        verifyPrivate(delegate).invoke("getPort");
+        verifyNoMoreInteractions(delegate);
+
+        assertEquals(expected, actual);
 
     }
 
