f7d9b61a3bcf346ca467fded2c8fd63135a2ebb9
bedrin <Dmitry.Bedrin@gmail.com>
2016-05-10 23:20:00 +0300

    PreparedStatementInvocationHandlerTest refactoring

--- src/test/java/io/sniffy/PreparedStatementInvocationHandlerTest.java
+++ src/test/java/io/sniffy/PreparedStatementInvocationHandlerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.OptionalInt;
 
 import static org.junit.Assert.*;
 
@@ -15,9 +15,7 @@ public class PreparedStatementInvocationHandlerTest extends BaseTest {
     @Test
     public void testExecuteBatch() throws Exception {
         try (Connection connection = openConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO PUBLIC.PROJECT (ID, NAME) VALUES (SEQ_PROJECT.NEXTVAL, ?)"
-             )) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PREPARED_STATEMENT)) {
             preparedStatement.setString(1, "foo");
             preparedStatement.addBatch();
             int[] result = preparedStatement.executeBatch();
@@ -27,26 +25,24 @@ public class PreparedStatementInvocationHandlerTest extends BaseTest {
 
     @Test
     public void testExecuteBatchCountUpdatedRows() throws Exception {
-        try (Spy $= Sniffer.expect(SqlQueries.exactRows(2));
+        try (@SuppressWarnings("unused") Spy $= Sniffer.expect(SqlQueries.exactRows(2));
              Connection connection = openConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO PUBLIC.PROJECT (ID, NAME) VALUES (SEQ_PROJECT.NEXTVAL, ?)"
-             )) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PREPARED_STATEMENT)) {
             preparedStatement.setString(1, "foo");
             preparedStatement.addBatch();
             preparedStatement.setString(1, "bar");
             preparedStatement.addBatch();
             int[] result = preparedStatement.executeBatch();
-            assertEquals(2, Arrays.stream(result).filter(i -> i != -1).reduce((a,b) -> a + b).getAsInt());
+            OptionalInt rowsAffected = Arrays.stream(result).filter(i -> i != -1).reduce((a, b) -> a + b);
+            assertTrue(rowsAffected.isPresent());
+            assertEquals(2, rowsAffected.getAsInt());
         }
     }
 
     @Test
     public void testExecuteEmptyBatch() throws Exception {
         try (Connection connection = openConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO PUBLIC.PROJECT (ID, NAME) VALUES (SEQ_PROJECT.NEXTVAL, ?)"
-             )) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PREPARED_STATEMENT)) {
             int[] result = preparedStatement.executeBatch();
             assertEquals(0, result.length);
         }
@@ -55,9 +51,7 @@ public class PreparedStatementInvocationHandlerTest extends BaseTest {
     @Test
     public void testExecuteInsertPreparedStatement() throws Exception {
         try (Connection connection = openConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO PUBLIC.PROJECT (ID, NAME) VALUES (SEQ_PROJECT.NEXTVAL, ?)"
-             )) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PREPARED_STATEMENT)) {
             preparedStatement.setString(1, "foo");
             int result = preparedStatement.executeUpdate();
             assertEquals(1, result);
