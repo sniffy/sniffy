ee348fbce2229c413e310df193f55199ea24220e
bedrin <Dmitry.Bedrin@gmail.com>
2016-05-10 23:16:29 +0300

    Added unit test for couting rows affected by batch updates

--- src/test/java/io/sniffy/PreparedStatementInvocationHandlerTest.java
+++ src/test/java/io/sniffy/PreparedStatementInvocationHandlerTest.java
@@ -1,9 +1,12 @@
 package io.sniffy;
 
+import io.sniffy.sql.SqlQueries;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +22,22 @@ public class PreparedStatementInvocationHandlerTest extends BaseTest {
             preparedStatement.addBatch();
             int[] result = preparedStatement.executeBatch();
             assertEquals(1, result.length);
+        }
+    }
+
+    @Test
+    public void testExecuteBatchCountUpdatedRows() throws Exception {
+        try (Spy $= Sniffer.expect(SqlQueries.exactRows(2));
+             Connection connection = openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO PUBLIC.PROJECT (ID, NAME) VALUES (SEQ_PROJECT.NEXTVAL, ?)"
+             )) {
+            preparedStatement.setString(1, "foo");
+            preparedStatement.addBatch();
+            preparedStatement.setString(1, "bar");
+            preparedStatement.addBatch();
+            int[] result = preparedStatement.executeBatch();
+            assertEquals(2, Arrays.stream(result).filter(i -> i != -1).reduce((a,b) -> a + b).getAsInt());
         }
     }
 
