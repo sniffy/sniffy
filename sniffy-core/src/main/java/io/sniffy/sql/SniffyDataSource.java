ae56c31f74d0d36f80cc45f98b79809411a097ce
bedrin <Dmitry.Bedrin@gmail.com>
2016-09-16 22:51:25 +0300

    Group network connetions made from DataSource.connect() methods under appropriate JDBC call

--- sniffy-core/src/main/java/io/sniffy/sql/SniffyDataSource.java
+++ sniffy-core/src/main/java/io/sniffy/sql/SniffyDataSource.java
@@ -1,7 +1,10 @@
 package io.sniffy.sql;
 
+import io.sniffy.Sniffy;
+
 import javax.sql.DataSource;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,6 +16,29 @@ import java.util.logging.Logger;
  */
 public class SniffyDataSource implements DataSource {
 
+    private final static Method GET_CONNECTION_METHOD;
+    private final static Method GET_CONNECTION_WITH_CREDENTIALS_METHOD;
+
+    static {
+        Method getConnectionMethod;
+        try {
+            getConnectionMethod = DataSource.class.getMethod("getConnection");
+        } catch (NoSuchMethodException e) {
+            getConnectionMethod = null;
+        }
+
+        GET_CONNECTION_METHOD = getConnectionMethod;
+
+        Method getConnectionWithCredentialsMethod;
+        try {
+            getConnectionWithCredentialsMethod = DataSource.class.getMethod("getConnection", String.class, String.class);
+        } catch (NoSuchMethodException e) {
+            getConnectionWithCredentialsMethod = null;
+        }
+
+        GET_CONNECTION_WITH_CREDENTIALS_METHOD = getConnectionWithCredentialsMethod;
+    }
+
     private final DataSource target;
 
     public SniffyDataSource(DataSource target) {
@@ -21,7 +47,16 @@ public class SniffyDataSource implements DataSource {
 
     @Override
     public Connection getConnection() throws SQLException {
-        Connection delegateConnection = target.getConnection();
+
+        Connection delegateConnection;
+        long start = System.currentTimeMillis();
+
+        try {
+            Sniffy.enterJdbcMethod();
+            delegateConnection = target.getConnection();
+        } finally {
+            Sniffy.exitJdbcMethod(GET_CONNECTION_METHOD, System.currentTimeMillis() - start);
+        }
 
         return Connection.class.cast(Proxy.newProxyInstance(
                 SniffyDriver.class.getClassLoader(),
@@ -32,7 +67,16 @@ public class SniffyDataSource implements DataSource {
 
     @Override
     public Connection getConnection(String username, String password) throws SQLException {
-        Connection delegateConnection = target.getConnection(username, password);
+
+        Connection delegateConnection;
+        long start = System.currentTimeMillis();
+
+        try {
+            Sniffy.enterJdbcMethod();
+            delegateConnection = target.getConnection(username, password);
+        } finally {
+            Sniffy.exitJdbcMethod(GET_CONNECTION_WITH_CREDENTIALS_METHOD, System.currentTimeMillis() - start);
+        }
 
         return Connection.class.cast(Proxy.newProxyInstance(
                 SniffyDriver.class.getClassLoader(),
--- sniffy-core/src/test/java/io/sniffy/sql/SniffyDataSourceTest.java
+++ sniffy-core/src/test/java/io/sniffy/sql/SniffyDataSourceTest.java
@@ -3,10 +3,14 @@ package io.sniffy.sql;
 import io.sniffy.BaseTest;
 import io.sniffy.Sniffy;
 import io.sniffy.Spy;
+import io.sniffy.socket.TcpConnections;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.lang.reflect.Proxy;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.sql.Connection;
 import java.sql.Statement;
 
@@ -27,6 +31,31 @@ public class SniffyDataSourceTest extends BaseTest {
             assertTrue(Proxy.isProxyClass(connection.getClass()));
             try (Spy spy = Sniffy.expect(SqlQueries.exactQueries(1));
                  Statement statement = connection.createStatement()) {
+                statement.execute("SELECT 1 FROM DUAL");
+                statement.getResultSet().next();
+            }
+        }
+    }
+
+    @Test
+    public void testGetConnectionWithSocketOperation() throws Exception {
+        JdbcDataSource h2DataSource = new JdbcDataSource();
+        h2DataSource.setURL("jdbc:h2:mem:");
+
+        JdbcDataSource targetDataSource = Mockito.spy(h2DataSource);
+
+        Mockito.when(targetDataSource.getConnection()).then(invocation -> {
+            Sniffy.logSocket("stackTrace", 1, new InetSocketAddress(InetAddress.getLoopbackAddress(), 9876), 2, 3, 4);
+            return invocation.callRealMethod();
+        });
+
+        SniffyDataSource sniffyDataSource = new SniffyDataSource(targetDataSource);
+
+        try (Spy spy = Sniffy.expect(SqlQueries.exactQueries(1)).expect(TcpConnections.none());
+             Connection connection = sniffyDataSource.getConnection()) {
+            assertNotNull(connection);
+            assertTrue(Proxy.isProxyClass(connection.getClass()));
+            try (Statement statement = connection.createStatement()) {
                 statement.execute("SELECT 1 FROM DUAL");
                 statement.getResultSet().next();
             }
