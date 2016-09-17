1aaec1f6ffa4554410836cf4b5f45092ee7ffd3d
bedrin <Dmitry.Bedrin@gmail.com>
2016-09-17 19:41:03 +0300

    More unit tests; fixed found issues

--- sniffy-core/src/test/java/io/sniffy/sql/SniffyDataSourceTest.java
+++ sniffy-core/src/test/java/io/sniffy/sql/SniffyDataSourceTest.java
@@ -5,13 +5,11 @@ import io.sniffy.Sniffy;
 import io.sniffy.Spy;
 import io.sniffy.Threads;
 import io.sniffy.socket.TcpConnections;
-import io.sniffy.util.ExceptionUtil;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import javax.sql.DataSource;
-import javax.sql.XAConnection;
 import javax.sql.XADataSource;
 import java.lang.reflect.Proxy;
 import java.net.InetAddress;
@@ -173,6 +171,58 @@ public class SniffyDataSourceTest extends BaseTest {
 
         try (Spy spy = Sniffy.expect(SqlQueries.exactQueries(1)).expect(TcpConnections.none());
              Connection connection = sniffyDataSource.getXAConnection("sa","sa").getConnection()) {
+            assertNotNull(connection);
+            assertTrue(Proxy.isProxyClass(connection.getClass()));
+            try (Statement statement = connection.createStatement()) {
+                statement.execute("SELECT 1 FROM DUAL");
+                statement.getResultSet().next();
+            }
+            assertTrue(spy.getSocketOperations(Threads.CURRENT, null, false).isEmpty());
+        }
+    }
+
+    @Test
+    public void testGetPooledConnectionWithSocketOperation() throws Exception {
+        JdbcDataSource h2DataSource = new JdbcDataSource();
+        h2DataSource.setURL("jdbc:h2:mem:");
+
+        JdbcDataSource targetDataSource = Mockito.spy(h2DataSource);
+
+        Mockito.when(targetDataSource.getXAConnection()).then(invocation -> {
+            Sniffy.logSocket("stackTrace", 1, new InetSocketAddress(InetAddress.getLoopbackAddress(), 9876), 2, 3, 4);
+            return invocation.callRealMethod();
+        });
+
+        SniffyDataSource sniffyDataSource = new SniffyDataSource(targetDataSource);
+
+        try (Spy spy = Sniffy.expect(SqlQueries.exactQueries(1)).expect(TcpConnections.none());
+             Connection connection = sniffyDataSource.getPooledConnection().getConnection()) {
+            assertNotNull(connection);
+            assertTrue(Proxy.isProxyClass(connection.getClass()));
+            try (Statement statement = connection.createStatement()) {
+                statement.execute("SELECT 1 FROM DUAL");
+                statement.getResultSet().next();
+            }
+            assertTrue(spy.getSocketOperations(Threads.CURRENT, null, false).isEmpty());
+        }
+    }
+
+    @Test
+    public void testGetPooledConnectionWithCredentialsWithSocketOperation() throws Exception {
+        JdbcDataSource h2DataSource = new JdbcDataSource();
+        h2DataSource.setURL("jdbc:h2:mem:");
+
+        JdbcDataSource targetDataSource = Mockito.spy(h2DataSource);
+
+        Mockito.when(targetDataSource.getXAConnection(anyString(), anyString())).then(invocation -> {
+            Sniffy.logSocket("stackTrace", 1, new InetSocketAddress(InetAddress.getLoopbackAddress(), 9876), 2, 3, 4);
+            return invocation.callRealMethod();
+        });
+
+        SniffyDataSource sniffyDataSource = new SniffyDataSource(targetDataSource);
+
+        try (Spy spy = Sniffy.expect(SqlQueries.exactQueries(1)).expect(TcpConnections.none());
+             Connection connection = sniffyDataSource.getPooledConnection("sa","sa").getConnection()) {
             assertNotNull(connection);
             assertTrue(Proxy.isProxyClass(connection.getClass()));
             try (Statement statement = connection.createStatement()) {
