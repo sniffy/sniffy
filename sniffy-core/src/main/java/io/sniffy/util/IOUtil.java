249e6bfe0fcec865a612288244f5843512a225b1
bedrin <Dmitry.Bedrin@gmail.com>
2016-11-03 00:10:06 +0300

    Added more unit tests for IOUtil

--- sniffy-core/src/main/java/io/sniffy/util/IOUtil.java
+++ sniffy-core/src/main/java/io/sniffy/util/IOUtil.java
@@ -1,29 +1,61 @@
 package io.sniffy.util;
 
+import sun.misc.IOUtils;
+
+import java.io.File;
+
 public class IOUtil {
 
     protected static final String MAIN_CLASS_PROPERTY_NAME = "sun.java.command";
     protected static final String WORKING_DIRECTORY_PROPERTY_NAME = "user.dir";
     protected static final String JAVA_CLASS_PATH_PROPERTY_NAME = "java.class.path";
+    protected static final String TEMP_DIRECTORY_PROPERTY_NAME = "java.io.tmpdir";
 
-    public static String getProcessID() {
+    protected static volatile String applicationId;
+    protected static volatile File applicationSniffyFolder;
 
-        StringBuilder sb = new StringBuilder();
+    public static File getApplicationSniffyFolder() {
 
-        String mainClassName = System.getProperty(MAIN_CLASS_PROPERTY_NAME);
-        if (null == mainClassName) mainClassName = "SniffyApplication";
+        if (null == applicationSniffyFolder) {
+            synchronized (IOUtils.class) {
+                if (null == applicationSniffyFolder) {
+                    File applicationSniffyFolder = new File(new File(System.getProperty(TEMP_DIRECTORY_PROPERTY_NAME)), getApplicationId());
+                    applicationSniffyFolder.mkdirs();
+                    IOUtil.applicationSniffyFolder = applicationSniffyFolder;
+                }
+            }
+        }
 
-        sb.append(mainClassName).append('.');
+        return applicationSniffyFolder;
 
-        String workingDir = System.getProperty(WORKING_DIRECTORY_PROPERTY_NAME);
-        String classPath = System.getProperty(JAVA_CLASS_PATH_PROPERTY_NAME);
+    }
 
-        int hashCode = workingDir != null ? workingDir.hashCode() : 0;
-        hashCode = 31 * hashCode + (classPath != null ? classPath.hashCode() : 0);
+    public static String getApplicationId() {
 
-        sb.append(Integer.toString(hashCode));
+        if (null == applicationId) {
+            synchronized (IOUtils.class) {
+                if (null == applicationId) {
+                    StringBuilder sb = new StringBuilder();
 
-        return sb.toString();
+                    String mainClassName = System.getProperty(MAIN_CLASS_PROPERTY_NAME);
+                    if (null == mainClassName) mainClassName = "SniffyApplication";
+
+                    sb.append(mainClassName).append('.');
+
+                    String workingDir = System.getProperty(WORKING_DIRECTORY_PROPERTY_NAME);
+                    String classPath = System.getProperty(JAVA_CLASS_PATH_PROPERTY_NAME);
+
+                    int hashCode = workingDir != null ? workingDir.hashCode() : 0;
+                    hashCode = 31 * hashCode + (classPath != null ? classPath.hashCode() : 0);
+
+                    sb.append(Integer.toString(hashCode));
+
+                    applicationId = sb.toString();
+                }
+            }
+        }
+
+        return applicationId;
 
     }
 
--- sniffy-core/src/test/java/io/sniffy/util/IOUtilTest.java
+++ sniffy-core/src/test/java/io/sniffy/util/IOUtilTest.java
@@ -24,35 +24,50 @@ public class IOUtilTest {
     @After
     public void restoreSystemProperties() {
         System.setProperties(backup);
+        resetIOUtilSingletons();
     }
 
     @Test
     public void getProcessID() throws Exception {
-        assertNotNull(IOUtil.getProcessID());
+        assertNotNull(IOUtil.getApplicationId());
     }
 
     @Test
     public void getProcessIDDependsOnWorkingDirectory() throws Exception {
         System.setProperty(IOUtil.WORKING_DIRECTORY_PROPERTY_NAME, "foo");
-        String pid1 = IOUtil.getProcessID();
+        resetIOUtilSingletons();
+        String pid1 = IOUtil.getApplicationId();
+
         System.setProperty(IOUtil.WORKING_DIRECTORY_PROPERTY_NAME, "bar");
-        String pid2 = IOUtil.getProcessID();
-        assertNotNull(pid1, pid2);
+        resetIOUtilSingletons();
+        String pid2 = IOUtil.getApplicationId();
+
+        assertNotEquals(pid1, pid2);
     }
 
     @Test
     public void getProcessIDDependsOnClassPath() throws Exception {
         System.setProperty(IOUtil.JAVA_CLASS_PATH_PROPERTY_NAME, "foo");
-        String pid1 = IOUtil.getProcessID();
+        resetIOUtilSingletons();
+        String applicationId1 = IOUtil.getApplicationId();
+
         System.setProperty(IOUtil.JAVA_CLASS_PATH_PROPERTY_NAME, "bar");
-        String pid2 = IOUtil.getProcessID();
-        assertNotNull(pid1, pid2);
+        resetIOUtilSingletons();
+        String applicationId2 = IOUtil.getApplicationId();
+
+        assertNotEquals(applicationId1, applicationId2);
     }
 
     @Test
     public void getProcessIDStartsWithMainClassName() throws Exception {
         System.setProperty(IOUtil.MAIN_CLASS_PROPERTY_NAME, "foo.bar.baz");
-        assertTrue(IOUtil.getProcessID().startsWith("foo.bar.baz."));
+        resetIOUtilSingletons();
+        assertTrue(IOUtil.getApplicationId().startsWith("foo.bar.baz."));
+    }
+
+    private void resetIOUtilSingletons() {
+        IOUtil.applicationId = null;
+        IOUtil.applicationSniffyFolder = null;
     }
 
 }
