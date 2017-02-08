19e7497d6112ae1b5e3ec653d4f8fdd270e9ed90
bedrin <Dmitry.Bedrin@gmail.com>
2017-02-09 00:47:19 +0300

    Added overview section to Sniffy documentation

---
+++ sniffy-test/sniffy-junit/src/test/java/io/sniffy/test/junit/usage/JUnitOverviewTest.java
@@ -0,0 +1,42 @@
+package io.sniffy.test.junit.usage;
+
+import io.sniffy.sql.SqlExpectation;
+import io.sniffy.sql.SqlStatement;
+import io.sniffy.sql.WrongNumberOfQueriesError;
+import io.sniffy.test.Count;
+import io.sniffy.test.junit.SniffyRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by bedrin on 09.02.2017.
+ */
+public class JUnitOverviewTest {
+    // tag::JUnitOverview[]
+    @Rule public SniffyRule sniffyRule = new SniffyRule();
+
+    @Test
+    @SqlExpectation(count = @Count(1))
+    public void testExpectedOneQueryGotOne() throws SQLException {
+        DriverManager.getConnection("sniffy:jdbc:h2:mem:", "sa", "sa").createStatement().execute("SELECT 1 FROM DUAL"); // <4>
+    }
+
+    @Rule public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    @SqlExpectation(count = @Count(max = 1), query = SqlStatement.SELECT)
+    public void testExpectedNotMoreThanOneSelectGotTwo() throws SQLException {
+        try (Statement statement = DriverManager.getConnection("sniffy:jdbc:h2:mem:", "sa", "sa").createStatement()) {
+            statement.execute("SELECT 1 FROM DUAL");
+            statement.execute("SELECT 2 FROM DUAL");
+        }
+        thrown.expect(WrongNumberOfQueriesError.class);
+    }
+    // end::JUnitOverview[]
+}
