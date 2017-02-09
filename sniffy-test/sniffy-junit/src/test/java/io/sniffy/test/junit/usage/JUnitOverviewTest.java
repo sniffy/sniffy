4dcd4c0f7ce717ca58e4b1ddad9cc03de92223a8
bedrin <Dmitry.Bedrin@gmail.com>
2017-02-10 00:35:57 +0300

    Removed unused imports; moved field declarations to the top

--- sniffy-test/sniffy-junit/src/test/java/io/sniffy/test/junit/usage/JUnitOverviewTest.java
+++ sniffy-test/sniffy-junit/src/test/java/io/sniffy/test/junit/usage/JUnitOverviewTest.java
@@ -9,25 +9,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- * Created by bedrin on 09.02.2017.
- */
 public class JUnitOverviewTest {
     // tag::JUnitOverview[]
     @Rule public SniffyRule sniffyRule = new SniffyRule();
+
+    @Rule public ExpectedException thrown = ExpectedException.none();
 
     @Test
     @SqlExpectation(count = @Count(1))
     public void testExpectedOneQueryGotOne() throws SQLException {
         DriverManager.getConnection("sniffy:jdbc:h2:mem:", "sa", "sa").createStatement().execute("SELECT 1 FROM DUAL"); // <4>
     }
-
-    @Rule public ExpectedException thrown = ExpectedException.none();
 
     @Test
     @SqlExpectation(count = @Count(max = 1), query = SqlStatement.SELECT)
