e0fb2708888934ff3ab3e5cde1b3107b8977f56d
bedrin <Dmitry.Bedrin@gmail.com>
2017-03-19 02:33:04 +0300

    Avoid unused imports such as 'java.util.stream.IntStream'

--- sniffy-core/src/test/java/io/sniffy/SniffyTest.java
+++ sniffy-core/src/test/java/io/sniffy/SniffyTest.java
@@ -11,7 +11,6 @@ import java.sql.Statement;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 
