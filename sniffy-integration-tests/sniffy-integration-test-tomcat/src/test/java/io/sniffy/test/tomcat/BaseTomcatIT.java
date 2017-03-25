6d81238267d7dd77a7480ad2cbf511390b792bf9
bedrin <Dmitry.Bedrin@gmail.com>
2017-03-25 18:23:12 +0300

    issues/319 Sniffy is injected incorrectly to pages with query parameters

---
+++ sniffy-integration-tests/sniffy-integration-test-tomcat/src/test/java/io/sniffy/test/tomcat/BaseTomcatIT.java
@@ -0,0 +1,43 @@
+package io.sniffy.test.tomcat;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import ru.yandex.qatools.allure.annotations.Issue;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BaseTomcatIT {
+
+    @Test
+    public void testSniffyInjected() {
+
+        WebDriver webDriver = new HtmlUnitDriver(true);
+
+        webDriver.navigate().to("http://127.0.0.1:8081/test");
+
+        assertFalse(webDriver.findElement(By.id("sniffy-iframe")).isDisplayed());
+        webDriver.findElement(By.className("sniffy-widget-icon-container")).click();
+        assertTrue(webDriver.findElement(By.id("sniffy-iframe")).isDisplayed());
+
+        webDriver.quit();
+    }
+
+    @Test
+    @Issue("issues/319")
+    public void testSniffyInjectedToUrlWithQueryParameters() {
+
+        WebDriver webDriver = new HtmlUnitDriver(true);
+
+        webDriver.navigate().to("http://127.0.0.1:8081/test?foo=bar");
+
+        assertFalse(webDriver.findElement(By.id("sniffy-iframe")).isDisplayed());
+        webDriver.findElement(By.className("sniffy-widget-icon-container")).click();
+        assertTrue(webDriver.findElement(By.id("sniffy-iframe")).isDisplayed());
+
+        webDriver.quit();
+    }
+
+}
