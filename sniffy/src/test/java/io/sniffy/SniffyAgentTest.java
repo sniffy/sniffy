9d3f63ccf49b979fbc26324d51c84c1d51e5c3ba
bedrin <Dmitry.Bedrin@gmail.com>
2017-03-26 16:36:25 +0300

    Added more unit tests

--- sniffy/src/test/java/io/sniffy/SniffyAgentTest.java
+++ sniffy/src/test/java/io/sniffy/SniffyAgentTest.java
@@ -43,7 +43,6 @@ public class SniffyAgentTest {
 
         entity = template.getForEntity("http://localhost:5555/connectionregistry/", String.class);
         assertTrue(entity.getStatusCode().is2xxSuccessful());
-        assertTrue(entity.getBody().contains("google.com"));
         assertEquals("google.com", JsonPath.read(entity.getBody(), "$.sockets[0].host"));
         assertEquals("443", JsonPath.read(entity.getBody(), "$.sockets[0].port"));
         assertEquals("OPEN", JsonPath.read(entity.getBody(), "$.sockets[0].status"));
@@ -52,10 +51,32 @@ public class SniffyAgentTest {
 
         entity = template.getForEntity("http://localhost:5555/connectionregistry/", String.class);
         assertTrue(entity.getStatusCode().is2xxSuccessful());
-        assertTrue(entity.getBody().contains("google.com"));
         assertEquals("google.com", JsonPath.read(entity.getBody(), "$.sockets[0].host"));
         assertEquals("443", JsonPath.read(entity.getBody(), "$.sockets[0].port"));
         assertEquals("CLOSED", JsonPath.read(entity.getBody(), "$.sockets[0].status"));
+    }
+
+    @Test
+    @Features("issues/327")
+    public void testAllowDataSource() {
+        TestRestTemplate template = new TestRestTemplate();
+
+        ResponseEntity<String> entity = template.postForEntity("http://localhost:5555/connectionregistry/datasource/jdbc:data:source/user", null, String.class);
+        assertTrue(entity.getStatusCode().is2xxSuccessful());
+
+        entity = template.getForEntity("http://localhost:5555/connectionregistry/", String.class);
+        assertTrue(entity.getStatusCode().is2xxSuccessful());
+        assertEquals("jdbc:data:source", JsonPath.read(entity.getBody(), "$.dataSources[0].url"));
+        assertEquals("user", JsonPath.read(entity.getBody(), "$.dataSources[0].userName"));
+        assertEquals("OPEN", JsonPath.read(entity.getBody(), "$.dataSources[0].status"));
+
+        template.delete("http://localhost:5555/connectionregistry/datasource/jdbc:data:source/user");
+
+        entity = template.getForEntity("http://localhost:5555/connectionregistry/", String.class);
+        assertTrue(entity.getStatusCode().is2xxSuccessful());
+        assertEquals("jdbc:data:source", JsonPath.read(entity.getBody(), "$.dataSources[0].url"));
+        assertEquals("user", JsonPath.read(entity.getBody(), "$.dataSources[0].userName"));
+        assertEquals("CLOSED", JsonPath.read(entity.getBody(), "$.dataSources[0].status"));
     }
 
     @Test
