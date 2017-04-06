c3caf1703e7fa26aeb3561a2d4a9c35df54660d8
bedrin <Dmitry.Bedrin@gmail.com>
2017-04-07 00:53:19 +0300

    Enabled SniffyStackoverflowApplicationTests

--- sniffy-core/src/test/java/io/sniffy/SniffyStackoverflowApplicationTests.java
+++ sniffy-core/src/test/java/io/sniffy/SniffyStackoverflowApplicationTests.java
@@ -1,24 +1,33 @@
 package io.sniffy;
 
+import io.sniffy.sql.SniffyDataSource;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+import ru.yandex.qatools.allure.annotations.Features;
 
+import javax.sql.DataSource;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SniffyStackoverflowApplicationTests
+public class SniffyStackoverflowApplicationTests extends BaseTest
 {
-	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	@Before
+	public void setupJdbcTemplate() {
+		JdbcDataSource h2DataSource = new JdbcDataSource();
+		h2DataSource.setURL("jdbc:h2:mem:");
+
+		DataSource wrap = SniffyDataSource.wrap(h2DataSource);
+
+		jdbcTemplate = new NamedParameterJdbcTemplate(wrap);
+	}
 
 	@Test
+	@Features("issues/331")
 	public void testStackOverflow()
 	{
 		jdbcTemplate.queryForList("SELECT * FROM DUAL WHERE NULL IN(:ids)",
