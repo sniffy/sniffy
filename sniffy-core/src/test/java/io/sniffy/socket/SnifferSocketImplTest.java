73115a31719350d2190b60e5d908cb1457bc5a25
bedrin <Dmitry.Bedrin@gmail.com>
2017-09-30 15:29:39 +0300

    Avoid unused imports such as 'org.mockito.Mockito'

--- sniffy-core/src/test/java/io/sniffy/socket/SnifferSocketImplTest.java
+++ sniffy-core/src/test/java/io/sniffy/socket/SnifferSocketImplTest.java
@@ -5,7 +5,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
