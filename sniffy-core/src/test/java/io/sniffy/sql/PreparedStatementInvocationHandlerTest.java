251bc32a5f5f3dccfb8ddf3fdf88579d7e4e970a
bedrin <Dmitry.Bedrin@gmail.com>
2017-10-19 00:07:56 +0300

    issues/335: SQL query counter doesn't support repeated queryes

--- sniffy-core/src/test/java/io/sniffy/sql/PreparedStatementInvocationHandlerTest.java
+++ sniffy-core/src/test/java/io/sniffy/sql/PreparedStatementInvocationHandlerTest.java
@@ -1,19 +1,55 @@
-package io.sniffy;
+package io.sniffy.sql;
 
-import io.sniffy.sql.SqlQueries;
+import io.sniffy.BaseTest;
+import io.sniffy.CurrentThreadSpy;
+import io.sniffy.Sniffy;
+import io.sniffy.Spy;
 import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Issue;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.lang.reflect.Proxy;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PreparedStatementInvocationHandlerTest extends BaseTest {
+
+    @Test
+    @Issue("issues/337")
+    public void testExecuteBatchSuccessNoInfo() throws SQLException, IOException {
+
+        PreparedStatement target = mock(PreparedStatement.class);
+        when(target.executeBatch()).thenReturn(new int[]{Statement.SUCCESS_NO_INFO, Statement.EXECUTE_FAILED});
+
+        PreparedStatement sniffyPreparedStatement = (PreparedStatement) Proxy.newProxyInstance(
+                PreparedStatementInvocationHandlerTest.class.getClassLoader(),
+                new Class[]{PreparedStatement.class},
+                new PreparedStatementInvocationHandler(target, null, "jdbc:test:connection:url", "sa", "UPDATE TAB SET FOO = ?")
+        );
+
+        try (CurrentThreadSpy spy = Sniffy.spyCurrentThread()) {
+
+            sniffyPreparedStatement.executeBatch();
+
+            List<SqlStats> sqlStatsList = new ArrayList<>(spy.getExecutedStatements().values());
+            assertEquals(1, sqlStatsList.size());
+
+            SqlStats sqlStats = sqlStatsList.get(0);
+
+            assertEquals(1, sqlStats.queries.intValue());
+            assertEquals(0, sqlStats.rows.intValue());
+
+        }
+
+    }
 
     @Test
     public void testExecuteBatch() throws Exception {
--- sniffy-web/src/main/java/io/sniffy/servlet/SniffyServlet.java
+++ sniffy-web/src/main/java/io/sniffy/servlet/SniffyServlet.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.URLDecoder;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Map;
