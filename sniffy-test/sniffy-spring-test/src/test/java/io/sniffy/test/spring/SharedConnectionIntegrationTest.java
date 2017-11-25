84611e330f5be052341d8ac9209e83670dd26186
bedrin <Dmitry.Bedrin@gmail.com>
2017-11-26 01:11:32 +0300

    Optimize imports

--- sniffy-test/sniffy-spring-test/src/test/java/io/sniffy/test/spring/SharedConnectionIntegrationTest.java
+++ sniffy-test/sniffy-spring-test/src/test/java/io/sniffy/test/spring/SharedConnectionIntegrationTest.java
@@ -1,25 +1,15 @@
 package io.sniffy.test.spring;
 
-import io.sniffy.BaseTest;
-import org.h2.jdbcx.JdbcDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextLoader;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
 import static org.junit.Assert.assertEquals;
