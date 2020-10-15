5d22518e4d22419a86ec0e6de7dc249c4e28f7a2
bedrin <Dmitry.Bedrin@gmail.com>
2020-10-15 22:49:31 +0300

    SniffyChannelCompatibilityTest

---
+++ sniffy-compatibility-tests/src/test/java/io/sniffy/SniffyChannelCompatibilityTest.java
@@ -0,0 +1,56 @@
+package io.sniffy;
+
+import io.sniffy.nio.SniffySelectorProvider;
+import io.sniffy.registry.ConnectionsRegistry;
+import io.sniffy.socket.EchoServerRule;
+import io.sniffy.socket.SnifferSocketImplFactory;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import static io.sniffy.Threads.*;
+import static io.sniffy.Threads.OTHERS;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+
+public class SniffyChannelCompatibilityTest {
+
+    protected final static byte[] RESPONSE = new byte[]{9,8,7,6,5,4,3,2};
+    protected final static byte[] REQUEST = new byte[]{1, 2, 3, 4};
+
+    protected static InetAddress localhost;
+
+    @Rule
+    public EchoServerRule echoServerRule = new EchoServerRule(RESPONSE);
+
+    @BeforeClass
+    public static void resolveLocalhost() throws UnknownHostException {
+        localhost = InetAddress.getByName(null);
+    }
+
+    @Test
+    public void testBlockSocketChannel() throws Exception {
+
+        try {
+            SniffySelectorProvider.install();
+            ConnectionsRegistry.INSTANCE.setSocketAddressStatus(localhost.getHostName(), echoServerRule.getBoundPort(), -1);
+
+            SocketChannel client = SocketChannel.open(new InetSocketAddress(localhost, echoServerRule.getBoundPort()));
+            client.write(ByteBuffer.wrap(REQUEST));
+
+            fail("Should have been blocked by Sniffy");
+        } catch (ConnectException e) {
+            assertTrue(e.getMessage().contains("refused by Sniffy"));
+        } finally {
+            ConnectionsRegistry.INSTANCE.clear();
+            SnifferSocketImplFactory.uninstall();
+        }
+
+    }
+
+}
