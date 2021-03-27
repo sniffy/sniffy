8f3122aebb89685dde0e38804e372a4e015323b4
bedrin <Dmitry.Bedrin@gmail.com>
2021-03-27 13:40:37 +0300

    Generate certificates in runtime for CaptureSslTrafficTest

---
+++ sniffy-module-tls/src/test/java/io/sniffy/tls/BaseSSLSocketTest.java
@@ -0,0 +1,69 @@
+package io.sniffy.tls;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.*;
+
+public class BaseSSLSocketTest {
+
+    private final TemporaryFolder tempFolder = new TemporaryFolder();
+    private final EchoSslServerRule echoServerRule = new EchoSslServerRule(tempFolder, RESPONSE);
+
+    @Rule
+    public TestRule chain = RuleChain
+            .outerRule(tempFolder)
+            .around(echoServerRule);
+
+    protected final static byte[] RESPONSE = new byte[]{9, 8, 7, 6, 5, 4, 3, 2};
+    protected final static byte[] REQUEST = new byte[]{1, 2, 3, 4};
+
+    protected static InetAddress localhost;
+
+    @BeforeClass
+    public static void resolveLocalhost() throws UnknownHostException {
+        localhost = InetAddress.getByName(null);
+    }
+
+    protected void performSocketOperation() {
+
+        try {
+            Socket socket = echoServerRule.getClientSSLContext().getSocketFactory().createSocket(localhost, echoServerRule.getBoundPort());
+            socket.setReuseAddress(true);
+
+            assertTrue(socket.isConnected());
+
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(REQUEST);
+            outputStream.flush();
+            socket.shutdownOutput();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InputStream inputStream = socket.getInputStream();
+            int read;
+            while ((read = inputStream.read()) != -1) {
+                baos.write(read);
+            }
+            socket.shutdownInput();
+
+            echoServerRule.joinThreads();
+
+            assertArrayEquals(REQUEST, echoServerRule.pollReceivedData());
+            assertArrayEquals(RESPONSE, baos.toByteArray());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+}
