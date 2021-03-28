15d1bacec51642bb7ab3770dde699af0a58bd655
bedrin <Dmitry.Bedrin@gmail.com>
2021-03-28 12:34:51 +0300

    SSLSocketAdapterMockitoTest

--- sniffy-core/src/test/java/io/sniffy/socket/SnifferInputStreamTest.java
+++ sniffy-core/src/test/java/io/sniffy/socket/SnifferInputStreamTest.java
@@ -3,7 +3,7 @@ package io.sniffy.socket;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
