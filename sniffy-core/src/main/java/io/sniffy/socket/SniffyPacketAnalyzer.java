147a40222f9e3f78d56a08a347e4d2cf5b8686a9
bedrin <dmitry.bedrin@gmail.com>
2022-02-06 21:25:15 +0300

    SniffyPacketAnalyzer.analyze now throws exceptions

--- sniffy-core/src/main/java/io/sniffy/socket/SniffyPacketAnalyzer.java
+++ sniffy-core/src/main/java/io/sniffy/socket/SniffyPacketAnalyzer.java
@@ -14,73 +14,66 @@ public class SniffyPacketAnalyzer {
         this.snifferSocket = snifferSocket;
     }
 
-    public void analyze(byte[] b, int off, int len) {
+    public void analyze(byte[] b, int off, int len) throws Exception {
 
         InetSocketAddress proxiedInetSocketAddress = null;
 
-        try {
-            @SuppressWarnings("CharsetObjectCanBeUsed") String potentialRequest = new String(b, off, len, Charset.forName("US-ASCII"));
+        @SuppressWarnings("CharsetObjectCanBeUsed") String potentialRequest = new String(b, off, len, Charset.forName("US-ASCII"));
 
-            // TODO: support CONNECT header sent in multiple small chunks
-            // TODO: support SOCKS proxy connection
-            int connectIx = potentialRequest.indexOf("CONNECT ");
+        // TODO: support CONNECT header sent in multiple small chunks
+        // TODO: support SOCKS proxy connection
+        int connectIx = potentialRequest.indexOf("CONNECT ");
 
-            if (0 == connectIx) {
-                int crIx = potentialRequest.indexOf("\r");
-                int lfIx = potentialRequest.indexOf("\n");
+        if (0 == connectIx) {
+            int crIx = potentialRequest.indexOf("\r");
+            int lfIx = potentialRequest.indexOf("\n");
 
-                int newLineIx;
+            int newLineIx;
 
-                if (crIx > 0) {
-                    if (lfIx > 0) {
-                        newLineIx = Math.min(crIx, lfIx);
-                    } else {
-                        newLineIx = lfIx;
-                    }
+            if (crIx > 0) {
+                if (lfIx > 0) {
+                    newLineIx = Math.min(crIx, lfIx);
                 } else {
-                    if (lfIx > 0) {
-                        newLineIx = lfIx;
+                    newLineIx = lfIx;
+                }
+            } else {
+                if (lfIx > 0) {
+                    newLineIx = lfIx;
+                } else {
+                    newLineIx = -1;
+                }
+            }
+
+            if (newLineIx > 0) {
+                int httpVersionIx = potentialRequest.substring(0, newLineIx).indexOf(" HTTP/");
+                if (httpVersionIx > 0) {
+
+                    String proxiedHostAndPort = potentialRequest.substring("CONNECT ".length(), httpVersionIx);
+
+                    String host;
+                    int port;
+
+                    if (proxiedHostAndPort.contains(":")) {
+                        host = proxiedHostAndPort.substring(0, proxiedHostAndPort.lastIndexOf(":"));
+                        port = Integer.parseInt(proxiedHostAndPort.substring(proxiedHostAndPort.lastIndexOf(":") + 1));
                     } else {
-                        newLineIx = -1;
+                        host = proxiedHostAndPort;
+                        port = 80;
                     }
-                }
 
-                if (newLineIx > 0) {
-                    int httpVersionIx = potentialRequest.substring(0, newLineIx).indexOf(" HTTP/");
-                    if (httpVersionIx > 0) {
+                    proxiedInetSocketAddress = new InetSocketAddress(host, port);
 
-                        String proxiedHostAndPort = potentialRequest.substring("CONNECT ".length(), httpVersionIx);
-
-                        String host;
-                        int port;
-
-                        if (proxiedHostAndPort.contains(":")) {
-                            host = proxiedHostAndPort.substring(0, proxiedHostAndPort.lastIndexOf(":"));
-                            port = Integer.parseInt(proxiedHostAndPort.substring(proxiedHostAndPort.lastIndexOf(":") + 1));
-                        } else {
-                            host = proxiedHostAndPort;
-                            port = 80;
-                        }
-
-                        proxiedInetSocketAddress = new InetSocketAddress(host, port);
-
-                    }
                 }
             }
-
-            if (null != proxiedInetSocketAddress) {
-                snifferSocket.setProxiedInetSocketAddress(proxiedInetSocketAddress);
-                ConnectionsRegistry.INSTANCE.resolveSocketAddressStatus(proxiedInetSocketAddress, snifferSocket);
-            }
-
-            // TODO: set flag to allow capturing proxied traffic
-            // TODO: think if we can capture decrypted traffic sent over proxied connection
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            snifferSocket.setFirstPacketSent(true);
         }
+
+        if (null != proxiedInetSocketAddress) {
+            snifferSocket.setProxiedInetSocketAddress(proxiedInetSocketAddress);
+            ConnectionsRegistry.INSTANCE.resolveSocketAddressStatus(proxiedInetSocketAddress, snifferSocket);
+        }
+
+        // TODO: set flag to allow capturing proxied traffic
+        // TODO: think if we can capture decrypted traffic sent over proxied connection
 
     }
 
