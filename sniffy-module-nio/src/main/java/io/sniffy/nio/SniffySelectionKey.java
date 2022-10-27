f13af7901619c433ca26e9a4978723db26bea398
bedrin <dmitry.bedrin@gmail.com>
2022-10-27 20:22:20 +0200

    Temporary disable forceJava14Compatibility

--- sniffy-module-nio/src/main/java/io/sniffy/nio/SniffySelectionKey.java
+++ sniffy-module-nio/src/main/java/io/sniffy/nio/SniffySelectionKey.java
@@ -19,7 +19,7 @@ public class SniffySelectionKey extends SelectionKey implements ObjectWrapper<Se
 
     static {
 
-        if (JVMUtil.getVersion() < 14 && !Boolean.getBoolean("io.sniffy.forceJava14Compatibility")) {
+        if (JVMUtil.getVersion() < 14 /*&& !Boolean.getBoolean("io.sniffy.forceJava14Compatibility")*/) {
 
             try {
                 AtomicReferenceFieldUpdater<SelectionKey, Object> defaultFieldUpdater = ReflectionUtil.getField(SelectionKey.class, null, "attachmentUpdater");
