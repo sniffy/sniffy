3fd66f288e8f09eae1446b848a7f1d258d84663e
bedrin <dmitry.bedrin@gmail.com>
2022-11-13 13:57:29 +0100

    Removed unused methods from JVMUtil

--- sniffy-core/src/main/java/io/sniffy/util/JVMUtil.java
+++ sniffy-core/src/main/java/io/sniffy/util/JVMUtil.java
@@ -1,42 +1,15 @@
 package io.sniffy.util;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @since 3.1.7
  */
 public class JVMUtil {
-
-    private final static Map<String, Boolean> CLASS_CACHE = new ConcurrentHashMap<String, Boolean>();
 
     /**
      * @return true if code is running inside sniffy tests; return false when used in other projects
      */
     public static boolean isTestingSniffy() {
         return null != System.getProperty("io.sniffy.test");
-    }
-
-    public static boolean hasJUnitOnClassPath() {
-        return hasClassAvailable("org.junit.Test");
-    }
-
-    public static boolean hasClassAvailable(String className) {
-        Boolean available = CLASS_CACHE.get(className);
-        if (null != available) {
-            return available;
-        } else {
-            synchronized (CLASS_CACHE) {
-                available = true;
-                try {
-                    Class.forName(className);
-                } catch (ClassNotFoundException e) {
-                    available = false;
-                }
-                CLASS_CACHE.put(className, available);
-            }
-        }
-        return available;
     }
 
     public static int getVersion() {
