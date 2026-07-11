befafa93f3405163133cc2347f4e68d0a0a8bce5
Dmitry Bedrin <dmitry.bedrin@gmail.com>
2026-07-11 21:44:10 +0200

    Replaced call to google.com with mock-server in one fo integration tests to stabilize it

--- sniffy-integration-tests/sniffy-integration-tests-decrypt-tls/src/test/java/io/sniffy/tls/DecryptGoogleTrafficTest.java
+++ sniffy-integration-tests/sniffy-integration-tests-decrypt-tls/src/test/java/io/sniffy/tls/DecryptGoogleTrafficTest.java
@@ -5,30 +5,85 @@ import io.sniffy.configuration.SniffyConfiguration;
 import io.sniffy.socket.AddressMatchers;
 import io.sniffy.socket.NetworkPacket;
 import io.sniffy.socket.SocketMetaData;
-import io.sniffy.util.JVMUtil;
-import io.sniffy.util.OSUtil;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.socket.PortFactory;
 
-import javax.net.ssl.SSLException;
-import java.io.IOException;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import static io.sniffy.socket.NetworkPacket.convertNetworkPacketsToString;
 import static org.junit.Assert.*;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 public class DecryptGoogleTrafficTest {
 
     @BeforeClass
-    public static void loadTlsModule() {
+    public static void loadTlsModule() throws Exception {
         SniffyConfiguration.INSTANCE.setDecryptTls(true);
         SniffyConfiguration.INSTANCE.setMonitorSocket(true);
         SniffyConfiguration.INSTANCE.setMonitorNio(true);
         SniffyConfiguration.INSTANCE.setPacketMergeThreshold(10000);
         Sniffy.initialize();
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[]{new TrustAllManager()}, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+    }
+
+    private ClientAndServer mockServer;
+
+    private int port = PortFactory.findFreePort();
+
+    @Before
+    public void startMockServer() {
+        mockServer = startClientAndServer(port);
+        mockServer.when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                );
+    }
+
+    @After
+    public void stopMockServer() {
+        mockServer.stop();
+    }
+
+    private static class TrustAllManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+
     }
 
     @Test
@@ -36,33 +91,15 @@ public class DecryptGoogleTrafficTest {
 
         try (Spy<?> spy = Sniffy.spy(SpyConfiguration.builder().captureNetworkTraffic(true).captureStackTraces(true).build())) {
 
-            for (int i = 0; i < 10; i++) {
+            URL url = new URL("https://localhost:" + port);
+            URLConnection urlConnection = url.openConnection();
 
-                try {
-                    URL url = new URL("https://www.google.com");
-                    URLConnection urlConnection = url.openConnection();
-
-                    // On Java 14 with parallel builds sometimes throws SSLException: An established connection was aborted by the software in your host machine
-                    //noinspection ResultOfMethodCallIgnored
-                    urlConnection.getInputStream().read();
-
-                    break;
-                } catch (IOException e) {
-                    if (e.getMessage().contains("An established connection was aborted by the software in your host machine") || e.getMessage().contains("Remote host terminated the handshake")
-                            && OSUtil.isWindows() && (JVMUtil.getVersion() == 14 || JVMUtil.getVersion() == 13)) {
-                        e.printStackTrace();
-                        System.err.println("Caught " + e + " exception on Java " + JVMUtil.getVersion() + " running on Windows; retrying in 2 seconds");
-                        Thread.sleep(2000);
-                    } else {
-                        throw e;
-                    }
-                }
-
-            }
+            //noinspection ResultOfMethodCallIgnored
+            urlConnection.getInputStream().read();
 
             Map<SocketMetaData, List<NetworkPacket>> decryptedNetworkTraffic = spy.getDecryptedNetworkTraffic(
                     Threads.CURRENT,
-                    AddressMatchers.exactAddressMatcher("www.google.com:443"),
+                    AddressMatchers.exactAddressMatcher("localhost:" + port),
                     GroupingOptions.builder().
                             groupByConnection(false).
                             groupByStackTrace(false).
@@ -78,7 +115,7 @@ public class DecryptGoogleTrafficTest {
             assertNotNull(entry.getKey());
             assertNotNull(entry.getValue());
 
-            assertEquals(2, entry.getValue().size());
+            assertEquals("Expected 2 packets, but instead got " + convertNetworkPacketsToString(entry.getValue()), 2, entry.getValue().size());
 
             NetworkPacket request = entry.getValue().get(0);
             NetworkPacket response = entry.getValue().get(1);
@@ -86,7 +123,7 @@ public class DecryptGoogleTrafficTest {
             assertEquals(true, request.isSent());
             assertEquals(false, response.isSent());
 
-            assertTrue(new String(request.getBytes(), Charset.forName("US-ASCII")).contains("Host: www.google.com"));
+            assertTrue(new String(request.getBytes(), Charset.forName("US-ASCII")).toLowerCase(Locale.ROOT).contains("host: localhost"));
             assertTrue(new String(response.getBytes(), Charset.forName("US-ASCII")).contains("200"));
 
         }
